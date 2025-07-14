b658d3123a0b9c4a76e4c6f3de16d99733df6c9a
Geshhanglimbu <geshhanglimbu@gmial.com>
2025-07-14 10:40:07 +0545

    protected route complete

--- hrmsextra/src/components/utils/protectedRoute.jsx
+++ hrmsextra/src/components/utils/protectedRoute.jsx
@@ -1,14 +1,14 @@
 import {Navigate} from "react-router-dom";
 
-export default function ProtectedRRoute({children, allowedRoles}){
+export default function ProtectedRRoute({children, allowedRoles=[]}){
     const token = localStorage.getItem("token");
 
     const user=JSON.parse(localStorage.getItem("user"));
     console.log(user.email);
 
-    if(!token || user){
-        return <Navigate to="/"/>;
-    }
+   if (!token || !user) {
+    return <Navigate to="/"/>;
+}
 
     if(allowedRoles.length > 0 && !allowedRoles.includes(user.role)){
         return <Navigate to="/fhome" replace />;
